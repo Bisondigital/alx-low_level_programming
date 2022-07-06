0120e8e63fa6e417f262ceab45bdf5fd95fbcde5
Bisondigital <jeremiahntuk50@gmail.com>
2022-07-06 14:47:19 -0700

    l

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
 * jack_bauer -> prints 24 hours
 */
@@ -27,12 +26,14 @@ _putchar(':')
 _putchar('0')
 _putchar(j + '0');
 }
-else if (j > 10)
+else if (j >= 10)
 {
 _putchar(':')
 _putchar((j / 10) + '0');
 _putchar((j % 10) + '0');
 }
+_putchar('\n');
 }
+
 }
 }
