b2ff87c0e878d5039cc3cf3c32d12ad658545519
Bisondigital <jeremiahntuk50@gmail.com>
2022-07-06 19:02:38 -0700

    sister

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,7 +10,7 @@ if (n >= 0 && n <= 15)
 {
 for (i = 0; i <= n; i++)
 {
-for (j = 0; j <= n; J++)
+for (j = 0; j <= n; j++)
 {
 k = j * i;
 if (j == 0)
@@ -38,7 +38,7 @@ else if (k >= 100)
 _putchar(',');
 _putchar(' ');
 _putchar((k / 100) + '0');
-_putchar(((k / 10) % 10) '0');
+_putchar((k / 10) % 10, '0');
 _putchar((k % 10) + '0');
 }
 }
