394f4156bc0fbb4994dd10cf78247d5e8d6d3431
Bisondigital <jeremiahntuk50@gmail.com>
2022-07-07 21:59:20 -0700

    k

---
+++ 0x03-debugging/3-print_remaining_days.c
@@ -0,0 +1,25 @@
+#include "main.h'
+#include <stdio.h>
+/**
+* print_remainin_days - takes a date and prints how many days are
+* left in the year, taking leap years into account 
+* @month: month in number format
+* @day: day of the month
+* @year: year
+* *rReturn#include "main.h'
+* #include <stdio.h>
+*
+*/* print_remainin_days - takes a date and prints how many days are
+left in the year, taking leap years into account 
+@month: month in number format
+@day: day of the month
+@year: year
+*rReturn#include "main.h'
+#include <stdio.h>
+/**
+* print_remainin_days - takes a date and prints how many days are
+* left in the year, taking leap years into account 
+* @month: month in number format
+* @day: day of the month
+* @year: year
+* *rReturn
