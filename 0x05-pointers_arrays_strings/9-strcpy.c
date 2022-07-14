f03e268896368e02aba016871a5f946a7e6c93c7
Bisondigital <jeremiahntuk50@gmail.com>
2022-07-13 20:07:53 -0700

    v

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* _strcpy - copy string
+* @dest: destination
+* @src: source
+* Return: Charater
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+int len, i;
+
+len = strlen(src);
+for (i = 0; i <= len; i++)
+{
+dest[i] = src[i];
+}
+return (dest);
+}
