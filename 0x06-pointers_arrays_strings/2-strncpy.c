0a042460be46a2a0f73e2d946ae92a87838884dd
Bisondigital <jeremiahntuk50@gmail.com>
2022-07-15 07:33:11 -0700

    vim

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,6 +1,5 @@
-#include "main,h"
+#include "main.h"
 #include <string.h>
-
 /**
 * *_strncpy - copying purpose
 * @dest: first parameter
@@ -8,7 +7,6 @@
 * @n: - third parameter
 * Return: string
 */
-
 char *_strncpy(char *dest, char *src, int n)
 {
 	strncpy(dest, src, n);
