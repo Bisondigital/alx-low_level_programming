e9dfa051628ec60732fec0d1528a7800b1f17914
Bisondigital <jeremiahntuk50@gmail.com>
2022-07-14 19:31:01 -0700

    kite

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,11 +1,13 @@
 #include "main.h"
+#include <string.h>
 
 /**
-* _srtcat -> this is a fuction strcat
+* _srtcat - this is a fuction that concatenates two strings
 * @dest: first param
 * @src: second param
 * Return: a string
 */
+
 char *_strcat(char *dest, char *src);
 {
 int len = 0, i;
@@ -13,11 +15,11 @@ int len = 0, i;
 while (dest[len])
 len++;
 
-for (i = 0; src[i] != '\0'; i++)
+for (i = 0; src[i] != 0; i++)
 {
 dest[len] = src[i];
 len += 1;
 }
 dest[len] = '\0';
-return (dest)
+return (dest);
 }
