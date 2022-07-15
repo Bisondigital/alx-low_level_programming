c4a68324a457e928e5f94849f7df2d113c61ef47
Bisondigital <jeremiahntuk50@gmail.com>
2022-07-15 07:19:16 -0700

    kit

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,14 +1,14 @@
 #include "main.h"
+#include <string.h>
 
 /**
 * *_strncat - function that concatenates two string
 * @dest: first parameter
 * @src: second parameter
-* @n: - third parameter
+* @n: third parameter
 * Return: string
 */
-
-char *_strncat(char *dest, char *src, int n);
+char *_strncat(char *dest, char *src, int n)
 {
 	strncat(dest, src, n);
 	return (dest);
