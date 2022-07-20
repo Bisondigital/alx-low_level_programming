571206ddc4b05950a676be324fb6dc049549ea0b
Bisondigital <jeremiahntuk50@gmail.com>
2022-07-19 20:13:54 -0700

    kit

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -7,10 +7,9 @@
 * @n: number of bytes to be copied
 * Return: string copied from source
 */
-char *_memcpy(char *dest, char *src, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 unsigned int a;
-
 for (a = 0; a < n; a++)
 dest[a] = src[a];
 return (dest);
