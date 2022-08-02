9517e6c3a2c9071f2b1353d40aad000bc05b85eb
Bisondigital <jeremiahntuk50@gmail.com>
2022-08-02 14:31:11 -0700

    kill

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+*free-dog - a function that frees dogs.
+*@d: pointer to dog struct.
+*
+*Return: void.
+*/
+void free _dog(dog_t *d)
+{
+if (d == 0)
+return;
+free(d->name);
+free(d->owner);
+free(d);
+}
